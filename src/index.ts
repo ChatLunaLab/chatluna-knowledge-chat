f795d190edfa6312ae30fb426d8aace9b4ce5bb6
dingyi <dingyi222666@foxmail.com>
2023-09-10 14:54:57 +0800

    build: add eslint

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,15 @@
 import { Context, Schema } from 'koishi'
 
-import { ChatHubPlugin } from "@dingyi222666/koishi-plugin-chathub/lib/services/chat"
+import { ChatHubPlugin } from '@dingyi222666/koishi-plugin-chathub/lib/services/chat'
 
-export function apply(context: Context, config: Config) {
-
-}
+export function apply(context: Context, config: Config) {}
 
 export const name = '@dingyi222666/chathub-knowledge-chat'
 
-export interface Config extends ChatHubPlugin.Config {
-
-}
+export interface Config extends ChatHubPlugin.Config {}
 
 export const Config = Schema.intersect([
-    Schema.object({
-
-    }).description('基础配置'),
+    Schema.object({}).description('基础配置')
 ]) as Schema<Config>
 
-
 export const using = ['chathub']
-
