2b459535a7becbf147c30f120b66bb19c8a60aba
dingyi <dingyi222666@foxmail.com>
2023-09-14 15:06:57 +0800

    feat: add csv document loader

---
+++ src/llm-core/document_loader/loaders/csv.ts
@@ -0,0 +1,16 @@
+import { Document } from 'langchain/dist/document'
+import { DocumentLoader, DocumentLoaderFields } from '../types'
+import { CSVLoader } from 'langchain/document_loaders/fs/csv'
+export default class CSVDocumentLoader extends DocumentLoader {
+    public load(path: string, fields: DocumentLoaderFields): Promise<Document[]> {
+        const loader = new CSVLoader(path)
+
+        return loader.load()
+    }
+
+    public async support(path: string): Promise<boolean> {
+        const ext = path.split('.').pop() || ''
+
+        return ext.toLowerCase() === 'csv'
+    }
+}
