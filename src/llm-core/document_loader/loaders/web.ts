a547c51bbca5ccfe3333c28d9bd0197e0113a3b8
dingyi <dingyi222666@foxmail.com>
2023-09-14 19:47:49 +0800

    feat: add web document loader

---
+++ src/llm-core/document_loader/loaders/web.ts
@@ -0,0 +1,18 @@
+import { Document } from 'langchain/dist/document'
+import { DocumentLoader, DocumentLoaderFields } from '../types'
+import { CheerioWebBaseLoader } from 'langchain/document_loaders/web/cheerio'
+
+export default class WebLoader extends DocumentLoader {
+    public load(path: string, fields: DocumentLoaderFields): Promise<Document[]> {
+        const loader = new CheerioWebBaseLoader(path, {
+            timeout: 1000 * 60 * 2
+        })
+
+        return loader.load()
+    }
+
+    public async support(path: string): Promise<boolean> {
+        // start with (http|https)://
+        return /^(http|https):\/\//.test(path)
+    }
+}
