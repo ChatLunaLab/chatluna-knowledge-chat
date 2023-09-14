7ee6cb7263e2bb04a5a55d9fcc9ba51236b497bc
dingyi <dingyi222666@foxmail.com>
2023-09-14 15:24:36 +0800

    feat: add pdf document loader

---
+++ src/llm-core/document_loader/loaders/pdf.ts
@@ -0,0 +1,17 @@
+import { Document } from 'langchain/dist/document'
+import { DocumentLoader, DocumentLoaderFields } from '../types'
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
+
+export default class PDFDocumentLoader extends DocumentLoader {
+    public load(path: string, fields: DocumentLoaderFields): Promise<Document[]> {
+        const loader = new PDFLoader(path)
+
+        return loader.load()
+    }
+
+    public async support(path: string): Promise<boolean> {
+        const ext = path.split('.').pop() || ''
+
+        return ext.toLowerCase() === 'pdf'
+    }
+}
